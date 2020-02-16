ff5b95552a9b5701a0800b27bc23b6fca6787067
Andrew Hardemon <aeroatlas@ymail.com>
2020-02-15 17:39:21 -0700

    html for adding comments and seeing thme is done

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,15 @@ $.getJSON("/articles", function(data){
     $("#articles").append(`<h1>${data[i].headline}</h1>`)
     $("#articles").append(`<h4>${data[i].summary}</h4>`)
     $("#articles").append(`<p>https://www.nytimes.com/${data[i].link}</p>`)
-    $("#articles").append(`<img src='${data[i].image}' href='https://www.nytimes.com/${data[i].link}'></img><hr><br>`)
+    $("#articles").append(`<img src='${data[i].image}' href='https://www.nytimes.com/${data[i].link}'></img><hr>`)
+    $("#articles").append(`
+    <form action="/submitComment" method="post">
+    <input type="text" id="yourComment" name="comment" placeholder="Give your Opinon!">
+    <button><input type="submit" id="submitComment"></button>
+    </form><br>
+    <p><button class="btn btn-primary" type="button" data-toggle="collapse" 
+    data-target="#commentsSection${i}" aria-expanded="false" aria-controls="collapseExample"> Comments </button></p>
+    <div class="collapse" id="commentsSection${i}"><div class="card card-body"></div></div><hr><br>
+`)
   }
 })
