471474e78e96592e19767c7b7f1915058c5e3c85
Andrew Hardemon <aeroatlas@ymail.com>
2020-02-21 17:29:02 -0700

    trying to get heroku to work

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articledb";
+const MONGODB_URI = process.env.MONGODB_URI2 || "mongodb://localhost/articledb";
 mongoose.connect(MONGODB_URI);
 
 // Requires
